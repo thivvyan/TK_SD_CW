07ea2e0f772c96ddec590313bee74b435ffea99c
thivvyan <thivvyank@gmail.com>
2024-11-25 16:32:48 +0000

    2nd comment

---
+++ add-products.php
@@ -0,0 +1,198 @@
+<?php
+
+session_start();
+@include 'config.php';
+
+// Check if admin is logged in
+if (!isset($_SESSION['admin_username'])) {
+    header("Location: orders.php");
+    exit;
+}
+
+// Handle logout
+if (isset($_GET['logout'])) {
+    session_destroy();
+    header("Location: admin.php");
+    exit;
+}
+
+if(isset($_POST['add_product'])){
+   $p_name = $_POST['p_name'];
+   $p_price = $_POST['p_price'];
+   $p_image = $_FILES['p_image']['name'];
+   $p_image_tmp_name = $_FILES['p_image']['tmp_name'];
+   $p_image_folder = 'uploaded_img/'.$p_image;
+
+   $insert_query = mysqli_query($conn, "INSERT INTO `product`(name, price, image) VALUES('$p_name', '$p_price', '$p_image')") or die('query failed');
+
+   if($insert_query){
+      move_uploaded_file($p_image_tmp_name, $p_image_folder);
+      $message[] = 'The product has been added succesfully';
+   }else{
+      $message[] = 'Sorry, we could not add the product';
+   }
+};
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_query = mysqli_query($conn, "DELETE FROM `product` WHERE product_id = $delete_id ") or die('query failed');
+   if($delete_query){
+      header('location:add-products.php');
+      $message[] = 'The product has been deleted';
+   }else{
+      header('location:add-products.php');
+      $message[] = 'The product could not be deleted';
+   };
+};
+
+if(isset($_POST['update_product'])){
+   $update_p_id = $_POST['update_p_id'];
+   $update_p_name = $_POST['update_p_name'];
+   $update_p_price = $_POST['update_p_price'];
+   $update_p_image = $_FILES['update_p_image']['name'];
+   $update_p_image_tmp_name = $_FILES['update_p_image']['tmp_name'];
+   $update_p_image_folder = 'uploaded_img/'.$update_p_image;
+
+   $update_query = mysqli_query($conn, "UPDATE `product` SET name = '$update_p_name', price = '$update_p_price', image = '$update_p_image' WHERE product_id = '$update_p_id'");
+
+   if($update_query){
+      move_uploaded_file($update_p_image_tmp_name, $update_p_image_folder);
+      $message[] = 'The product has been updated succesfully';
+      header('location:add-products.php');
+   }else{
+      $message[] = 'Sorry, the product could not be updated';
+      header('location:add-products.php');
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Orders</title>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/orders.css">
+</head>
+<body>
+
+<!-- Header -->
+<div class="header">
+   <img src="images/logo1.png" alt="logo">
+   <div class="admin-info">
+    <button onclick="window.location.href='customers.php'" class="btn mr-50">View Customers</button>
+    <button onclick="window.location.href='orders.php'" class="btn mr-50">View Orders</button>
+       <span>Welcome, <?php echo htmlspecialchars($_SESSION['admin_username']); ?></span>
+       <a href="?logout=true">Logout</a>
+   </div>
+</div>
+
+<!-- Sub-header -->
+<div class="sub-header">
+   <h2>Add Products</h2>
+</div>
+
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '<div class="message"><span>'.$message.'</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i> </div>';
+   };
+};
+
+?>
+
+<div class="container">
+
+<section>
+
+<form action="" method="post" class="add-product-form" enctype="multipart/form-data">
+   <h3>add a new product</h3>
+   <input type="text" name="p_name" placeholder="enter the product name" class="box" required>
+   <input type="number" name="p_price" min="0" placeholder="enter the product price" class="box" required>
+   <input type="file" name="p_image" accept="image/png, image/jpg, image/jpeg" class="box" required>
+   <input type="submit" value="add the product" name="add_product" class="btn">
+</form>
+
+</section>
+
+<section class="display-product-table">
+
+   <table>
+
+      <thead>
+         <th>product image</th>
+         <th>product name</th>
+         <th>product price</th>
+         <th>action</th>
+      </thead>
+
+      <tbody>
+         <?php
+         
+            $select_products = mysqli_query($conn, "SELECT * FROM `product`");
+            if(mysqli_num_rows($select_products) > 0){
+               while($row = mysqli_fetch_assoc($select_products)){
+         ?>
+
+         <tr>
+            <td><img src="uploaded_img/<?php echo $row['image']; ?>" height="100" alt=""></td>
+            <td><?php echo $row['name']; ?></td>
+            <td>$<?php echo $row['price']; ?>/-</td>
+            <td>
+               <a href="add-products.php?delete=<?php echo $row['product_id']; ?>" class="delete-btn" onclick="return confirm('Are your sure you want to delete this?');"> <i class="fas fa-trash"></i> delete </a>
+               <a href="add-products.php?edit=<?php echo $row['product_id']; ?>" class="option-btn"> <i class="fas fa-edit"></i> update </a>
+            </td>
+         </tr>
+
+         <?php
+            };    
+            }else{
+               echo "<div class='empty'>no product added</div>";
+            };
+         ?>
+      </tbody>
+   </table>
+
+</section>
+
+<section class="edit-form-container">
+
+   <?php
+   
+   if(isset($_GET['edit'])){
+      $edit_id = $_GET['edit'];
+      $edit_query = mysqli_query($conn, "SELECT * FROM `product` WHERE product_id = $edit_id");
+      if(mysqli_num_rows($edit_query) > 0){
+         while($fetch_edit = mysqli_fetch_assoc($edit_query)){
+   ?>
+
+   <form action="" method="post" enctype="multipart/form-data">
+      <img src="uploaded_img/<?php echo $fetch_edit['image']; ?>" height="200" alt="">
+      <input type="hidden" name="update_p_id" value="<?php echo $fetch_edit['product_id']; ?>">
+      <input type="text" class="box" required name="update_p_name" value="<?php echo $fetch_edit['name']; ?>">
+      <input type="number" min="0" class="box" required name="update_p_price" value="<?php echo $fetch_edit['price']; ?>">
+      <input type="file" class="box" required name="update_p_image" accept="image/png, image/jpg, image/jpeg">
+      <input type="submit" value="update the prodcut" name="update_product" class="btn">
+      <input type="reset" value="cancel" id="close-edit" class="option-btn">
+   </form>
+
+   <?php
+            };
+         };
+         echo "<script>document.querySelector('.edit-form-container').style.display = 'flex';</script>";
+      };
+   ?>
+
+</section>
+
+</div>
+
+
+</body>
+</html>
